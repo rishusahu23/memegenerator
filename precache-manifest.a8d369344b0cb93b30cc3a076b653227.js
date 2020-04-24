0c18f79a5c401247030b4a169b83661774af3899
Rishu Sahu <rishusahu23@gmail.con>
2020-04-24 12:19:32 +0530

    Updates

---
+++ precache-manifest.a8d369344b0cb93b30cc3a076b653227.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef7e7947080c4907abd22820845ed605",
+    "url": "/memegenerator/index.html"
+  },
+  {
+    "revision": "44bd298db9f9f9b2be48",
+    "url": "/memegenerator/static/css/main.65ddddc8.chunk.css"
+  },
+  {
+    "revision": "404c9ab3d49091c8e121",
+    "url": "/memegenerator/static/js/2.52ba13eb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/memegenerator/static/js/2.52ba13eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44bd298db9f9f9b2be48",
+    "url": "/memegenerator/static/js/main.ffac5086.chunk.js"
+  },
+  {
+    "revision": "56ed1b294c1f6cd5c04f",
+    "url": "/memegenerator/static/js/runtime-main.729435f8.js"
+  }
+]);
