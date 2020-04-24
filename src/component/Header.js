6c4bdd0a80125cce867de7fa0e50571dc595de0d
Rishu Sahu <rishusahu23@gmail.con>
2020-04-24 12:09:03 +0530

    memegenerator completed

---
+++ src/component/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Header extends React.Component{
+	render(){
+		return(<div>
+			<header>
+				<img  src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
+					alt="problem"/>
+				<h1>Meme-Generator</h1>
+			</header>
+		</div>)
+	}
+}
+
+
+export default Header
